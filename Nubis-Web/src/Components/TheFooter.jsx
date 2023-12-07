5b66c715e35cb5a33001b84ff2704fd3afa34d5d
Jan1Verse1 <oluwaseyiadedotun@gmail.com>
2023-12-07 17:22:32 +0100

    last fix on images

--- Nubis-Web/src/Components/TheFooter.jsx
+++ Nubis-Web/src/Components/TheFooter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/Images/logo.png";
+import logo from "../assets/images/logo.png";
 import "./TheFooter.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
